e824af5a74452899cc49056053f932990303702d
Ian White <ianwhite@utexas.edu>
2014-05-20 17:41:39 -0500

    initialize routes and templates in app.js

--- dev/client/bucketListApp/www/js/app.js
+++ dev/client/bucketListApp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic'm 'bucketList.controllers', 'bucketList.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,53 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('auth', {
+      url: "/auth",
+      abstract: true,
+      templateUrl: 'templates/auth.html'
+    })
+    .state('auth.signin', {
+      url: '/signin',
+      views: {
+        'auth-signin': {
+          templateUrl: 'templates/auth-signin.html',
+          controller: 'SignInCtrl'
+        }
+      }
+    })
+    .state('auth.signup' {
+      url: '/signup',
+      views: {
+        'auth-singup': {
+          templateUrl: 'templates/auth-signup.html',
+          controller: 'SignUpCtrl'
+        }
+      }
+    })
+    .state('bucket', {
+      url: "/bucket",
+      abstract: true,
+      template: 'templates/bucket.html'
+    })
+    .state('bucket-list', {
+      url: '/list',
+      views: {
+        'bucket-list': {
+          templateUrl: 'templates/bucket-list.html',
+          controller: 'myListCtrl'
+        }
+      }
+    })
+    .state('bucket.completed', {
+      url: '/completed',
+      views: {
+        'bucket-completed': {
+          templateUrl: 'templates/bucket-completed.html',
+          controller: 'completedCtrl'
+        }
+      }
+    })
+  $urlRouterProvider.otherwise('/auth/signin');
+});
