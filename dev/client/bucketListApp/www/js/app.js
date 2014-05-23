109026268304bce67696c59964ba9a22d7f493f3
Ian White <ianwhite@utexas.edu>
2014-05-23 14:04:24 -0500

    clean up typos

--- dev/client/bucketListApp/www/js/app.js
+++ dev/client/bucketListApp/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'm 'bucketList.controllers', 'bucketList.services'])
+angular.module('starter', ['ionic', 'bucketList.controllers', 'bucketList.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -33,7 +33,7 @@ angular.module('starter', ['ionic'm 'bucketList.controllers', 'bucketList.servic
         }
       }
     })
-    .state('auth.signup' {
+    .state('auth.signup', {
       url: '/signup',
       views: {
         'auth-singup': {
